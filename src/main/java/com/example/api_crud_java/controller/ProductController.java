eae5e211d24d33d5309b6385a64d12a714f674ce
SebastianHinestroza12 <menas7527@gmail.com>
2024-10-13 22:46:42 -0500

    feat: add ProductController with CRUD endpoints
    - Add ProductController class with endpoints for CRUD operations.
    - Configure endpoint mappings for ProductController.
    - Set response status codes for create and delete operations.

---
+++ src/main/java/com/example/api_crud_java/controller/ProductController.java
@@ -0,0 +1,46 @@
+package com.example.api_crud_java.controller;
+
+import com.example.api_crud_java.entity.Product;
+import com.example.api_crud_java.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product createProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        product.setId(id);
+        return productService.updateProduct(product);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+}
